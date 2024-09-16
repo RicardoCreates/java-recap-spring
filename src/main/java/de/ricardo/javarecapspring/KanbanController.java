2f3b2992e672882d7759538d477fd64cf738e5d5
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-09-16 15:50:39 +0200

    Develop backend

--- src/main/java/de/ricardo/javarecapspring/KanbanController.java
+++ src/main/java/de/ricardo/javarecapspring/KanbanController.java
@@ -2,13 +2,40 @@ package de.ricardo.javarecapspring;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/todo")
 @RequiredArgsConstructor
 public class KanbanController {
 
+    public final KanbanService kanbanService;
+
+    @GetMapping
+    public List<Kanban> getKanbans(){
+        return  kanbanService.getKanbans();
+    }
+
+    @GetMapping("/{id}")
+    public Kanban getKanbanById(@PathVariable String id) {
+        return kanbanService.getKanbanById(id);
+    }
+
+    @PostMapping
+    public Kanban createKanban(@RequestBody Kanban kanban){
+        return kanbanService.createKanban(kanban);
+    }
+
+    @PutMapping("/{id}")
+    public Kanban updateKanban(@RequestBody Kanban kanban, @PathVariable String id){
+        return kanbanService.updateKanban(kanban, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteKanban(@PathVariable String id){
+        kanbanService.deleteKanban(id);
+    }
 
 }
--- src/main/java/de/ricardo/javarecapspring/KanbanService.java
+++ src/main/java/de/ricardo/javarecapspring/KanbanService.java
@@ -4,9 +4,48 @@ package de.ricardo.javarecapspring;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class KanbanService {
 
+    private final KanbanRepo kanbanRepo;
+    private final idService idService;
+
+    public List<Kanban> getKanbans(){
+        List<Kanban> kanbans = kanbanRepo.findAll();
+        return kanbans;
+    }
+
+    public Kanban getKanbanById(String id){
+        return kanbanRepo.findById(id).orElse(null);
+    }
+
+    public Kanban createKanban(Kanban kanban){
+        Kanban kanbanToAdd = new Kanban(
+                idService.generateId(),
+                kanban.description(),
+                kanban.status()
+        );
+        return kanbanRepo.save(kanbanToAdd);
+    }
+
+    public Kanban updateKanban(Kanban kanban, String id){
+        if (kanbanRepo.existsById(id)){
+            kanbanRepo.deleteById(id);
+            Kanban kanbanUpdate = new Kanban(
+                    idService.generateId(),
+                    kanban.description(),
+                    kanban.status()
+            );
+            return kanbanRepo.save(kanbanUpdate);
+        }
+        return null;
+    }
+
+    public void deleteKanban(String id){
+        kanbanRepo.deleteById(id);
+    }
 
 }
